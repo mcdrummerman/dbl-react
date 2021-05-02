4d4e07044f53e45df120570a7b611dc7dfbe50d2
Rob Toftness <rob.toftness@fourwindsinteractive.com>
2021-05-02 15:21:09 -0600

    add blog icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,14 +4,14 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (<footer className="container">
     <div className="row">
-    <div className="col-xs-2">
+      <div className="col-xs-2 mx-2 mb-1">
         <a target="_blank" rel="noopener noreferrer" title="Facebook" href="https://www.facebook.com/groups/345613403039899">
 
           <i className="fab fa-facebook-square fa-4x" aria-hidden="true"></i>
 
         </a>
       </div>
-      <div className="col-xs-2">
+      <div className="col-xs-2 mx-2 mb-1">
         <a target="_blank" rel="noopener noreferrer" title="twitter" href="https://twitter.com/DenBicycleLobby">
 
           <i className="fab fa-twitter fa-4x" aria-hidden="true"></i>
@@ -19,10 +19,18 @@ const Footer: React.FC = () => {
         </a>
 
       </div>
-      <div className="col-xs-2">
+      <div className="col-xs-2 mx-2 mb-1">
         <a target="_blank" rel="noopener noreferrer" title="Youtube" href="https://www.youtube.com/channel/UCPubgXuLvHhBEYWzCnM2_xQ">
 
           <i className="fab fa-youtube fa-4x" aria-hidden="true"></i>
+
+        </a>
+
+      </div>
+      <div className="col-xs-2 mx-2 mb-1">
+        <a target="_blank" rel="noopener noreferrer" title="Blog" href="http://blog.dbl.bike">
+
+          <i className="fas fa-blog fa-4x" aria-hidden="true"></i>
 
         </a>
 

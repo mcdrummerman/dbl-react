4e8bb48c9cc90e857e679e4a69bf87db8df0e434
Rob Toftness <rob.toftness@fourwindsinteractive.com>
2022-03-17 14:00:03 -0600

    add insta icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,6 +20,14 @@ const Footer: React.FC = () => {
 
       </div>
       <div className="col-xs-2 mx-2 mb-1">
+        <a target="_blank" rel="noopener noreferrer" title="instagram" href="https://www.instagram.com/denverbicyclelobby/">
+
+          <i className="fab fa-instagram fa-4x" aria-hidden="true"></i>
+
+        </a>
+
+      </div>
+      <div className="col-xs-2 mx-2 mb-1">
         <a target="_blank" rel="noopener noreferrer" title="Youtube" href="https://www.youtube.com/channel/UCPubgXuLvHhBEYWzCnM2_xQ">
 
           <i className="fab fa-youtube fa-4x" aria-hidden="true"></i>
